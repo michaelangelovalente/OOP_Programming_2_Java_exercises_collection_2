739cb7ea5a6a48a822497f95239803e747a060c2
michaelangelovalente <miky10.mv@gmail.com>
2023-02-28 19:50:01 +0100

    first commit

---
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -0,0 +1,8 @@
+public class Program{
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest){
+            int result = 0;
+            
+            for( int i = fromWhere; i <  )
+            return 0;
+    }
+}
